299486c2b835b247bc100017bb31a35b78e8c93a
Aaron1234517 <22371455@buaa.edu.cn>
2024-09-11 20:15:12 +0800

    init project

---
+++ src/route/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from "vue-router";
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/home',
+            name: 'home',
+            component: () => import('../views/one.vue')
+        }
+    ]
+})
+// 导出
+export default router
