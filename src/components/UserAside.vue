d127fecd142b5c05c01377089f86c4642f8434af
wangsixiang <22373342>
2024-09-17 19:21:41 +0800

    haha

--- src/components/UserAside.vue
+++ src/components/UserAside.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import {Document, Lock, Setting, UserFilled} from '@element-plus/icons-vue'
 </script>
 
 <template>
-  <el-col :span="4">
+  <el-col>
     <el-menu
         active-text-color="#ffd04b"
         background-color="#ffffff"
@@ -12,40 +12,33 @@
         text-color="#000000"
         @open="handleOpen"
         @close="handleClose"
+        style="width:250px"
     >
-      <el-sub-menu index="1">
-        <template #title>
-          <el-icon><location /></el-icon>
-          <span>Navigator One</span>
-        </template>
-        <el-menu-item-group title="Group One">
-          <el-menu-item index="1-1">item one</el-menu-item>
-          <el-menu-item index="1-2">item two</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group Two">
-          <el-menu-item index="1-3">item three</el-menu-item>
-        </el-menu-item-group>
-        <el-sub-menu index="1-4">
-          <template #title>item four</template>
-          <el-menu-item index="1-4-1">item one</el-menu-item>
-        </el-sub-menu>
-      </el-sub-menu>
-      <el-menu-item index="2">
-        <el-icon><icon-menu /></el-icon>
-        <span>Navigator Two</span>
+      <el-menu-item index="1" >
+        <el-icon><UserFilled /></el-icon>
+        <span class="fontStyle">个人信息</span>
       </el-menu-item>
-      <el-menu-item index="3" disabled>
+      <el-menu-item index="2">
+        <el-icon><Lock /></el-icon>
+        <span class="fontStyle">安全隐私</span>
+      </el-menu-item>
+      <el-menu-item index="3">
         <el-icon><document /></el-icon>
-        <span>Navigator Three</span>
+        <span class="fontStyle">论文管理</span>
       </el-menu-item>
       <el-menu-item index="4">
         <el-icon><setting /></el-icon>
-        <span>Navigator Four</span>
+        <span class="fontStyle">成果统计</span>
       </el-menu-item>
     </el-menu>
   </el-col>
 </template>
 
 <style scoped>
-
+.fontStyle{
+  font-size: larger;
+  text-align: center;
+  display: flex;
+  align-items: center;
+}
 </style>
--- src/views/user.vue
+++ src/views/user.vue
@@ -1,10 +1,13 @@
 <template>
 
-  <UserAside></UserAside>
+
 
   <div class="common-layout">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="250px">
+        <UserAside></UserAside>
+      </el-aside>
+
       <el-main>Main</el-main>
     </el-container>
   </div>
