9f2f3db65c4792b22e1f0972d83c7c1046fb42d0
wangsixiang <22373342>
2024-09-19 00:27:09 +0800

    主页面能够更好的Ciallo～(∠・▽< )⌒☆

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,18 +1,32 @@
 <script setup>
-  import {ref} from "vue";
-
+import {onMounted, ref} from "vue";
   const searchType = ref("主题");
+
+  const fullText = "Ciallo, World～(∠・▽< )⌒☆";
+  const displayedText = ref(""); // 动态显示的文字
+
+  onMounted(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      if (index < fullText.length) {
+        displayedText.value += fullText[index];
+        index++;
+      } else {
+        clearInterval(interval); // 打字完成后停止
+      }
+    }, 150); // 设置打字速度，150ms显示一个字符
+  });
 </script>
 
 <template>
 <div class="input">
   <div style="margin-bottom: 5%">
-    <h2 class="title">Make Academic Visible</h2>
+    <h2 class="title">{{displayedText}}</h2>
   </div>
-  <div>
-  <el-input style="height: 40px; max-width: 60%" placeholder="请输入搜索内容">
+  <div style="background-color: transparent">
+  <el-input style="height: 40px; max-width: 60%;" placeholder="请输入搜索内容">
     <template #prepend>
-      <el-select v-model="searchType" style="width: 115px">
+      <el-select v-model="searchType" style="width: 115px;background-color: transparent">
         <el-option label="主题" value="1" />
         <el-option label="篇名" value="2" />
         <el-option label="关键词" value="3" />
@@ -25,4 +39,5 @@
 
 <style scoped>
   @import "@/css/main.css";
+
 </style>
