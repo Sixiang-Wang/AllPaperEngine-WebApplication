d2c649a94c1703df526da838ad7045b87e0f948f
Aaron1234517 <22371455@buaa.edu.cn>
2024-09-19 00:05:40 +0800

    init main view

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -6,7 +6,11 @@
 
 <template>
 <div class="input">
-  <el-input style="height: 40px" placeholder="请输入搜索内容">
+  <div style="margin-bottom: 5%">
+    <h2 class="title">Make Academic Visible</h2>
+  </div>
+  <div>
+  <el-input style="height: 40px; max-width: 60%" placeholder="请输入搜索内容">
     <template #prepend>
       <el-select v-model="searchType" style="width: 115px">
         <el-option label="主题" value="1" />
@@ -15,6 +19,7 @@
       </el-select>
     </template>
   </el-input>
+  </div>
 </div>
 </template>
 
