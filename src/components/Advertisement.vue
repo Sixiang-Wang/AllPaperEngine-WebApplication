66d235ae3945d0fa1f610f5fed8837669f80de42
Aaron1234517 <22371455@buaa.edu.cn>
2024-09-18 17:09:20 +0800

    add an advertisement

---
+++ src/components/Advertisement.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <el-card style="max-width: 80%">
+    <div class="ad-header">
+      <span>申请成为学者</span>
+    </div>
+    <p class="ad-text">向我们发送申请，成为学者，更好地管理论文、查看论文动态</p>
+    <el-divider/>
+    <div class="ad-button">
+      <el-button type="primary" plain>点击此处申请成为学者</el-button>
+    </div>
+  </el-card>
+</template>
+
+<style scoped>
+@import "@/css/ad.css";
+</style>
--- src/views/user/UserSecurity.vue
+++ src/views/user/UserSecurity.vue
@@ -1,66 +1,74 @@
 <script setup>
-  import {ArrowRight} from "@element-plus/icons-vue";
+import {ArrowRight} from "@element-plus/icons-vue";
+import Advertise from "@/components/Advertisement.vue";
 
-  const tableData = [
-    {
-      feature: "账号名",
-      value: "1145141919810"
-    },
-    {
-      feature: "密码",
-      value: "**********"
-    },
-    {
-      feature: "密保问题",
-      value: ""
-    },
-    {
-      feature: "电子邮箱",
-      value: "912948333@qq.com"
-    }
-  ]
-    const tableData2 = [
-      {
-        feature: "个人信息公开"
-      },
-      {
-        feature: "登录时邮箱验证"
-      }
-    ]
+const tableData = [
+  {
+    feature: "账号名",
+    value: "1145141919810"
+  },
+  {
+    feature: "密码",
+    value: "**********"
+  },
+  {
+    feature: "密保问题",
+    value: ""
+  },
+  {
+    feature: "电子邮箱",
+    value: "912948333@qq.com"
+  }
+]
+const tableData2 = [
+  {
+    feature: "个人信息公开"
+  },
+  {
+    feature: "登录时邮箱验证"
+  }
+]
 </script>
 
 <template>
-  <el-card style="max-width: 800px">
-    <div class="card-header">
-      <span>账号安全</span>
+  <div style="display: flex">
+    <div>
+      <el-card style="max-width: 800px">
+        <div class="card-header">
+          <span>账号安全</span>
+        </div>
+        <el-table :data="tableData" style="width: 100%" show-header="false">
+          <el-table-column prop="feature" width="100px"></el-table-column>
+          <el-table-column prop="value" width="600px"></el-table-column>
+          <el-table-column width="50px">
+            <template #default="scope">
+              <arrow-right style="width: 25px; height: 25px;"/>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
+      <el-card style="max-width: 800px;margin-top: 5%">
+        <div class="card-header">
+          <span>账号安全</span>
+        </div>
+        <el-table :data="tableData2" style="width: 100%" show-header="false">
+          <el-table-column prop="feature" width="700px"></el-table-column>
+          <el-table-column prop="value" width="70px">
+            <template #default="scope">
+              <el-switch
+                  v-model="scope.row.value"
+                  class="ml-2"
+                  style="--el-switch-on-color: #13ce66;"
+              />
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
     </div>
-    <el-table :data="tableData" style="width: 100%" show-header="false">
-      <el-table-column prop="feature" width="100px"></el-table-column>
-      <el-table-column prop="value" width="600px"></el-table-column>
-      <el-table-column width="50px">
-        <template #default="scope">
-          <arrow-right style="width: 25px; height: 25px;"/>
-        </template>
-      </el-table-column>
-    </el-table>
-  </el-card>
-  <el-card style="max-width: 800px;margin-top: 5%">
-    <div class="card-header">
-      <span>账号安全</span>
+    <div style="margin-left: 5%">
+      <Advertise/>
     </div>
-    <el-table :data="tableData2" style="width: 100%" show-header="false">
-      <el-table-column prop="feature" width="700px"></el-table-column>
-      <el-table-column prop="value" width="70px">
-        <template #default="scope">
-          <el-switch
-              v-model="scope.row.value"
-              class="ml-2"
-              style="--el-switch-on-color: #13ce66;"
-          />
-        </template>
-      </el-table-column>
-    </el-table>
-  </el-card>
+  </div>
 </template>
 
 <style scoped>
