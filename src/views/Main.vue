95f0b9492e592de540b8c3fc3bed2e1d5d7164ba
wangsixiang <22373342>
2024-09-19 15:21:36 +0800

    修改了搜索按钮 Ciallo～(∠・▽< )⌒☆

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {onMounted, ref} from "vue";
+import {Search} from '@element-plus/icons-vue'
   const searchType = ref("主题");
   const searchInput = ref("");
   const fullText = "Ciallo, World～(∠・▽< )⌒☆";
@@ -14,8 +15,12 @@ import {onMounted, ref} from "vue";
       } else {
         clearInterval(interval); // 打字完成后停止
       }
-    }, 100); // 设置打字速度，150ms显示一个字符
+    }, 150); // 设置打字速度，150ms显示一个字符
   });
+
+const clickSearch = () => {
+
+};
 </script>
 
 <template>
@@ -24,7 +29,7 @@ import {onMounted, ref} from "vue";
     <h2 class="title">{{displayedText}}</h2>
   </div>
   <div style="background-color: transparent !important;">
-  <el-input v-model="searchInput" style="height: 40px; max-width: 60%;" placeholder="请输入搜索内容">
+  <el-input v-model="searchInput" class="search-input" placeholder="请输入搜索内容">
     <template #prepend>
       <el-select v-model="searchType" style="width: 115px">
         <el-option label="主题" value="1" />
@@ -32,43 +37,13 @@ import {onMounted, ref} from "vue";
         <el-option label="关键词" value="3" />
       </el-select>
     </template>
-    <template #append>
-      <el-button>搜索</el-button>
-    </template>
+
   </el-input>
+    <el-button :icon="Search" @click="clickSearch" class="search-button"  />
   </div>
 </div>
 </template>
 
 <style scoped>
-
   @import "@/css/main.css";
-  :deep(.el-input-group__append) {
-    background-color: transparent !important;
-    color: rgba(255, 255, 255, 0.7);
-  }
-  :deep(.el-input__wrapper){
-    background-color:rgba(0,0,0,0) !important;
-    --el-input-focus-border-color:#FFFFFF;
-  }
-  :deep(.el-input-group__prepend){
-    background-color:rgba(0,0,0,0);
-  }
-  :deep(.el-input__inner) {
-    background-color: rgba(0, 0, 0, 0) !important;
-    color: #fff;
-  }
-  :deep(.el-input__inner::placeholder) {
-    color: rgba(255, 255, 255, 0.7) !important;
-    cursor: pointer;
-  }
-  :deep(.el-select__selected-item) {
-    color: rgba(255, 255, 255, 0.7) !important;
-    cursor: pointer;
-  }
-/*不要碰这里的代码！别放到.css中*/
-  :deep(.el-select__wrapper){
-    height: 40px;
-    --el-input-focus-border-color: #FFFFFF;
-  }
 </style>
