45d3438366c0c12974e26f6b021d537798f9b2ff
Yu Jiachen <1430186906@qq.com>
2024-11-07 21:30:21 +0800

    环境配置

--- src/main/java/com/example/scholar/dao/WorkMapper.java
+++ src/main/java/com/example/scholar/dao/WorkMapper.java
@@ -25,7 +25,7 @@ public interface WorkMapper {
     List<Work> selectWorkByKeywords(String word,int frompage,int topage);
     @Select("select count(*) from openalex_works where match(title) against(#{word})")
     int getWorkLengthByTitle(String word);
-    @Update("update openalex_works set keywords_text = #{keywords},abstract=#{abstractText} where id = #{id}")
+    @Update("update openalex_works set keywordsText = #{keywords},abstract=#{abstractText} where id = #{id}")
     void insertKeywordsAndAbstract(String id,String keywords,String abstractText);
 
 
