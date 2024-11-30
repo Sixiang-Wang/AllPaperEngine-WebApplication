e634607b99685f7763438aea041e2520d869a51c
Aaron1234517 <22371455@buaa.edu.cn>
2024-11-30 12:34:58 +0800

    一揽子更新

--- src/api/http.js
+++ src/api/http.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import store from '@/api/index.js'
 
-let baseUrl="http://localhost:1145/";
+let baseUrl="http://116.204.112.5:1145/";
 // 创建axios实例
 const httpService = axios.create({
   // url前缀-'http:xxx.xxx'
