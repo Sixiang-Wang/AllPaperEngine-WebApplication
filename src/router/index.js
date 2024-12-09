22fd27565ae145624ff18a557c03f4a3f1ba2da5
Aaron1234517 <22371455@buaa.edu.cn>
2024-12-09 17:23:08 +0800

    修改路由为hash模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path: "/",
