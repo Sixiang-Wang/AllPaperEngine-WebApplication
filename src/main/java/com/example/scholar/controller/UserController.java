b02903c0ca2a655e23a6498ec79487a5e8a0dbd5
xiaobing0222 <taosirui-20050218@qq.com>
2024-12-24 12:33:04 +0800

    修改

--- src/main/java/com/example/scholar/controller/UserController.java
+++ src/main/java/com/example/scholar/controller/UserController.java
@@ -615,7 +615,7 @@ public class UserController {
     @DateTimeFormat
     public R addHistory(@RequestParam int userId,
                         @RequestParam String publicationId,
-                        @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime timestamp) {
+                        @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime timestamp) {
         try {
             HashMap<String, Object> resultMap = userService.addHistory(userId, publicationId, timestamp);
             if ("浏览历史添加成功".equals(resultMap.get("msg"))) {
