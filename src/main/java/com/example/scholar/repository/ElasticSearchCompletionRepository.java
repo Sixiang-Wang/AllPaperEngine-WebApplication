5cc9e4a75a4b7b9ca00ec91a06bad2320bd5e0b2
Yu Jiachen <1430186906@qq.com>
2024-12-24 23:19:54 +0800

    delete Repository

--- src/main/java/com/example/scholar/repository/ElasticSearchCompletionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.scholar.repository;
-import com.example.scholar.domain.openalexElasticsearch.Works;
-import com.example.scholar.domain.openalexElasticsearch.WorksCompletion;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-public interface ElasticSearchCompletionRepository extends ElasticsearchRepository<WorksCompletion, String> {
-}
